8bddc10b56cccc16f154ee626f76a2cd9982efbe
Richard Newcombe <hirich1@hotmail.com>
2022-06-23 10:05:03 +1000

    zombie name
    zombie name is now working in the new zombie file

--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// ui nameSpace
+using UnityEngine.UI;
 
-public class Zombie : MonoBehaviour 
+public class Zombie : MonoBehaviour
 {
+    // making ref to text box
+    [SerializeField] private Text textbox;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("name" + Singleton.Instance.text);
+        if(textbox != null)
+        {
+            textbox.text = "Welcome " + Singleton.Instance.text;
+        }
+        
+        
+       // Debug.Log("name" + Singleton.Instance.text);
     }
 
     // Update is called once per frame
